267a14eac8662aede0036c5452d8ae5663a23613
hzq <13402318457@163.com>
2018-09-03 09:41:08 +0800

    新增版本选择

--- bin/finlean.js
+++ bin/finlean.js
@@ -10,25 +10,26 @@ const chalk = require('chalk');
 const ora = require('ora');
 program
     .version('0.1.0')
-    .option('i, init', '初始化finlin项目')
+    .option('i, init', '初始化finlean项目')
     .parse(process.argv);
 const nameQuestion = {
     type: 'input',
-    message: `项目名称: `,
+    message: '项目名称: ',
     name: 'name',
-    default: 'finlin'
+    default: 'finlean'
 };
 const versionQuestion = {
     type: 'input',
-    message: `初始版本: `,
+    message: '项目版本(web/pc): ',
     name: 'version',
-    default: '0.0.1'
+    default: 'pc'
 };
 if (program.init) {
     console.info('');
-    inquirer.prompt([nameQuestion, versionQuestion]).then(function(answers) {
-        const spinner = ora('正在从github下载fl').start();
-        download('MrHzq/fl', answers.name, function(err) {
+    inquirer.prompt([nameQuestion, versionQuestion]).then(answers => {
+        const spinner = ora('正在下载模板').start();
+        let _download = 'MrHzq/finlean_' + answers.version;
+        download(_download, answers.name, err => {
             if (!err) {
                 spinner.clear();
                 console.info('');
@@ -41,9 +42,11 @@ if (program.init) {
                 spinner.succeed(['项目创建成功,请继续进行以下操作:']);
                 console.info('');
                 console.info(chalk.cyan(` -  cd ${answers.name}`));
-                console.info(chalk.cyan(` -  npm install / yarn`));
+                console.info(chalk.cyan(` -  npm install`));
                 console.info(chalk.cyan(` -  npm start / npm run dev`));
             } else {
+                spinner.succeed(['项目创建失败,请重新创建']);
+                console.info('项目创建失败,请重新创建');
                 // 可以输出一些项目失败的信息
             }
         });
