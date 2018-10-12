e503ca4099d81ff3fd79a121d978a6f5ac30999f
hzq <13402318457@163.com>
2018-10-12 15:17:43 +0800

    增加使用方式

--- bin/finlean.js
+++ bin/finlean.js
@@ -7,7 +7,7 @@ const download = require('download-git-repo');
 const chalk = require('chalk');
 const ora = require('ora');
 program
-    .version('2.0.2')
+    .version('2.0.3')
     .option('i, init', '初始化finlean项目')
     .parse(process.argv);
 const promptList = [
