304da6f9c33478185e0e34558bb94d413c15151d
huangzhiqiang02 <huangzhiqiang02@zuoyebang.com>
2024-08-16 18:25:39 +0800

    refactor: break change

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+// 上述为 Node.js 脚本文件的行首注释，告知使用 node 来解析和执行后续的脚本内容
+
+// 引入 commander 模块，官方使用文档：https://github.com/tj/commander.js/blob/HEAD/Readme_zh-CN.md
+const { program } = require("commander");
+
+const version = require("../package.json").version;
+
+program.version(version);
+
+const cmdList = require("./cmdList.json");
+
+cmdList.forEach((item) => {
+  const { cmd, alias, desc } = item;
+  // 定义命令与参数，类似 hzq addMsg 等等
+  program
+    .command(cmd)
+    .alias(alias)
+    .description(desc)
+    .action((_, options) => {
+      require(`../lib/${cmd}`)(_, options);
+    });
+});
+
+program.parse(process.argv); // 解析用户输入的命令和参数，第一个参数是要解析的字符串数组，第二个参数是解析选项
---
+++ config/handler.js
@@ -0,0 +1,30 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+const fileName = "global.json";
+
+// 读取配置
+const readConfig = () => {
+  const configPath = path.join(__dirname, fileName);
+  try {
+    const configData = JSON.parse(fs.readFileSync(configPath));
+    return configData;
+  } catch (err) {
+    return null;
+  }
+};
+
+// 写入配置
+const writeConfig = (config) => {
+  const configPath = path.join(__dirname, fileName);
+  try {
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+  } catch (err) {
+    console.error(`写入配置文件时出错: ${err.message}`);
+  }
+};
+
+module.exports = {
+  readConfig,
+  writeConfig,
+};
