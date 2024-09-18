2d8df12459e2471827311d29fc4084325f2fe7ba
huangzhiqiang02 <huangzhiqiang02@zuoyebang.com>
2024-09-18 14:00:31 +0800

    feat: add one cmd

--- bin/index.js
+++ bin/index.js
@@ -17,16 +17,27 @@ log.success(`welcome use ${name}@${version} ~`, [true, true]);
 
 program.version(version);
 
-sortBy(cmdList, "cmd")
-  .filter((item) => item.cmd)
-  .forEach((item) => {
-    const { cmd, alias, _description } = item;
-    // 定义命令与参数，类似 hzq addMsg 等等
+if (cmdList.length > 1) {
+  sortBy(cmdList, "cmd")
+    .filter((item) => item.cmd)
+    .forEach((item) => {
+      const { cmd, alias, _description } = item;
+      // 定义命令与参数，类似 hzq addMsg 等等
+      program
+        .command(cmd)
+        .alias(alias)
+        .description(_description)
+        .action(actionMain);
+    });
+} else {
+  const item = cmdList[0];
+  if (item) {
+    // 当只有一个命令时，则不需要 command 配置，直接使用
     program
-      .command(cmd)
-      .alias(alias)
-      .description(_description)
-      .action(actionMain);
-  });
+      .alias(item.alias)
+      .description(item._description)
+      .action((_, options) => actionMain(_, { ...options, cmd: item.cmd }));
+  }
+}
 
 program.parse(process.argv); // 解析用户输入的命令和参数，第一个参数是要解析的字符串数组，第二个参数是解析选项
